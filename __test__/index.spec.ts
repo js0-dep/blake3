7ca99fa8bb77f32f93c265477328063c5f38ee5c
js0.site <jssite@googlegroups.com>
2025-11-14 15:37:27 +0800

    .

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,10 +1,11 @@
 import test from "ava";
 
-import { blake } from "../index";
+import { blake3 } from "../index";
 
 test("sync function from native code", (t) => {
-  const hash = blake("hello");
-  console.log(hash);
-  // const fixture = 42
-  // t.is(plus100(fixture), fixture + 100)
+  const hash = blake3("hello").toString("hex");
+  t.is(
+    hash,
+    "ea8f163db38682925e4491c5e58d4bb3506ef8c14eb78a86e908c5624a67200f",
+  );
 });
